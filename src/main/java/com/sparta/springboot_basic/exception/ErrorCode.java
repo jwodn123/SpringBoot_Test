483fe4e5ccaea5f9af9849d8599ff7fca616ed06
JEON <jwodn123@naver.com>
2023-05-02 13:54:40 +0900

    Modify : ErrorCode 수정

---
+++ src/main/java/com/sparta/springboot_basic/exception/ErrorCode.java
@@ -0,0 +1,29 @@
+package com.sparta.springboot_basic.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ExceptionStatus {
+
+    //BAD_REQUEST
+    USER_Duplicate(HttpStatus.BAD_REQUEST,"중복된 username 입니다.", 400),
+    TOKEN_NOT_FOUND(HttpStatus.BAD_REQUEST,"토큰이 유효하지 않습니다.", 400),
+    WRITER_ONLY_MODIFY(HttpStatus.BAD_REQUEST,"작성자만 수정할 수 있습니다.", 400),
+    WRITER_ONLY_DELETE(HttpStatus.BAD_REQUEST,"작성자만 삭제할 수 있습니다.", 400),
+
+    //NOT_FOUND
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND,"회원을 찾을 수 없습니다.", 400),
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND,"게시글을 찾을 수 없습니다.", 400),
+    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND,"댓글을 찾을 수 없습니다.", 400);
+
+
+
+    private final String errorMessage;
+    private final int statusCode;
+
+    ExceptionStatus(String errorMessage, int statusCode) {
+        this.errorMessage = errorMessage;
+        this.statusCode = statusCode;
+    }
+}
